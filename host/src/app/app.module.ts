bfc6a52f92c730886e388d1227f349936a374c11
Baskar K <kbaskar0812@gmail.com>
2023-09-22 08:51:33 +0530

    added material theme to host

--- host/src/app/app.module.ts
+++ host/src/app/app.module.ts
@@ -4,12 +4,17 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@omx-tools/material';
+
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    MaterialModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
